9e4f4095e50a7e8eb336690f4b4036512e70a561
ilovehacking92 <mafarfaoua@insea.ac.ma>
2022-01-20 03:45:15 +0100

    fix a minor bug

--- src/main/java/com/microservices/tp/moviecatalogueservice/MovieCatalogueServiceApplication.java
+++ src/main/java/com/microservices/tp/moviecatalogueservice/MovieCatalogueServiceApplication.java
@@ -2,6 +2,7 @@ package com.microservices.tp.moviecatalogueservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
@@ -9,6 +10,7 @@ import org.springframework.web.client.RestTemplate;
 public class MovieCatalogueServiceApplication {
 
 	@Bean
+	@LoadBalanced
 	public RestTemplate getRestTemplate() { return new RestTemplate();}
 	public static void main(String[] args) {
 		SpringApplication.run(MovieCatalogueServiceApplication.class, args);
