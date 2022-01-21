4ebccb312f72d8e4217977a2f7bf6c102bd64807
1zinebRhosne1 <54914775+1zinebRhosne1@users.noreply.github.com>
2022-01-21 16:13:35 +0100

    Update MovieCatalogRessource.java

--- src/main/java/resources/MovieCatalogRessource.java
+++ src/main/java/resources/MovieCatalogRessource.java
@@ -1,5 +1,6 @@
 package resources;
 import java.lang.reflect.Array;
+import java.lang.reflect.ParameterizedType;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
@@ -8,6 +9,8 @@ import java.util.stream.Collectors;
 import models.CatalogItem;
 import models.Movie;
 import models.Rating;
+import models.UserRating;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,16 +19,18 @@ import org.springframework.web.client.RestTemplate;
 @RestController
 @RequestMapping("/catalog")
 public class MovieCatalogRessource {
+    @Autowired
+    private RestTemplate restTemplate;
     @RequestMapping("/{userId}")
     public List<CatalogItem>getCatalog(@PathVariable("userId") String userId) {
-        RestTemplate restTemplate = new RestTemplate();
 
-        List<Rating> ratings = Arrays.asList(
-                new Rating("1234", 4),
-                new Rating("1265", 4)//array of bunch of ratings
-        );
-        return ratings.stream().map(rating ->{
-                Movie movie = restTemplate.getForObject("localhost:8082/movies/"+rating.getMovieId(), Movie.class);//takes two objects
+
+        UserRating ratings = restTemplate.getForObject("http://localhost:8083/ratingsdata/users/"+userId, UserRating.class);
+
+        return ratings.getUserRating().stream().map(rating ->{
+            //pour chaque movieID,fait appel à movie info et get the details
+                 Movie movie = restTemplate.getForObject("localhost:8081/movies/"+rating.getMovieId(), Movie.class);//takes two objects
+            //rassembler le tt
                 return new CatalogItem(movie.getName(), "Test", rating.getRating()) ;
     })
                  .collect(Collectors.toList());
